0783b2e3fcb9896e5137d7bf7452e208cc130162
sleepingcephalopoda <sleepingcephalopoda@users.noreply.github.com>
2018-02-12 18:56:03 -0400

    Added header file.

---
+++ Chemistry.h
@@ -0,0 +1,49 @@
+#ifndef CHEMISTRY_H
+#define CHEMISTRY_H
+
+#include <string>
+
+using namespace std;
+
+// Class Definition
+
+// Introduction To Experimental Chemistry
+class CHEM1125Lab00
+
+{
+
+// Public interface.
+public:
+
+	// Creating an object that needs a default constructor.
+	CHEM1125Lab00();
+
+	// Constructor that initializes experiment information.
+	CHEM1125Lab00(string, int, int);
+
+	// Compound
+	void setCompoundName(string);
+	string getCompoundName();
+
+	// Initial Weight
+	void setInitialWeight(int);
+	int getInitialWeight();
+
+	// Set Melting Point
+	void setMeltingPoint(int);
+	int getMeltingPoint();
+
+	// Show compound details.
+	void getDetails();
+
+// Private interface.
+private:
+
+	string CompoundName;
+	int ClassInitialWeight;
+	int ClassMeltingPoint;
+
+
+};
+
+#endif
